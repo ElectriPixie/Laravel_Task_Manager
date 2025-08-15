1069bb6ba32617c6266bd9f02b5aa6afc558605d
ElectriPixie <ElectriPixie@gmail.com>
2025-08-15 18:07:38 -0400

    updated basic layout to include scripts

--- tasker/resources/views/layouts/app.blade.php
+++ tasker/resources/views/layouts/app.blade.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="UTF-8">
     <title>Tasker</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body>
     @yield('content')
+    @yield('scripts')
 </body>
 </html>
