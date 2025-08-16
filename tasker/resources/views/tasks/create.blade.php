b46c9a05875713f93543d338ad83628feef30961
ElectriPixie <ElectriPixie@gmail.com>
2025-08-15 20:25:03 -0400

    update the tasks create.blade.php

--- tasker/resources/views/tasks/create.blade.php
+++ tasker/resources/views/tasks/create.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Create Task</h1>
+<h1>Create Task for {{ $project->name }}</h1>
 
-<form action="{{ route('tasks.store') }}" method="POST">
+<form action="{{ route('projects.tasks.store', $project) }}" method="POST">
     @csrf
     <input type="text" name="name" placeholder="Task name" required>
-    <button type="submit">Create</button>
+    <button type="submit">Create Task</button>
 </form>
 
-<a href="{{ route('tasks.index') }}">Back to list</a>
+<a href="{{ route('projects.index', ['project_id' => $project->id]) }}">Back to Project</a>
 @endsection
